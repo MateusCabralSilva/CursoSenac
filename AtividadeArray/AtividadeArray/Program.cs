17cd35a3e4b910b8e978f4bb703bdf76971ac9c2
Mateus Cabral da Silva <131392438+MateusCabralSilva@users.noreply.github.com>
2026-02-13 14:27:24 -0300

    Add files via upload

---
+++ AtividadeArray/AtividadeArray/Program.cs
@@ -0,0 +1,23 @@
+﻿double[] nt = new double[5];
+double soma = 0;
+
+for (int i = 0; i < nt.Length; i++)
+{
+    Console.Write($"Digite a {i + 1}º nota: ");
+    nt[i] = double.Parse(Console.ReadLine());
+    soma += nt[i];
+}
+double media = soma / nt.Length;
+
+if (media >= 8)
+{
+    Console.WriteLine($"Media: {media:f2} \nSituação: Aprovado");
+}
+else if (media >= 5 && media < 8)
+{
+    Console.WriteLine($"Media: {media:f2} \nSituação: Recuperação");
+}
+else
+{
+    Console.WriteLine($"Media: {media:f2} \nSituação: Reprovado");
+}
