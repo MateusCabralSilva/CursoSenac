c58f40b2ab395ce747f46b1474b88778508d2971
Mateus Cabral da Silva <131392438+MateusCabralSilva@users.noreply.github.com>
2026-02-13 14:31:06 -0300

    Add files via upload

---
+++ Estacionamento/Estacionamento/Program.cs
@@ -0,0 +1,39 @@
+﻿int horas,tipo,preco=0;
+double desconto,total;
+
+Console.Write("Digite quantas horas ficou na vaga: ");
+horas = int.Parse(Console.ReadLine());
+if (horas > 0)
+{
+    Console.WriteLine("===== Tipo de veiculo =====");
+    Console.WriteLine("1.Carro\n2.Moto\n3.caminhonete");
+    Console.Write("Escolha um opção: ");
+    tipo = int.Parse(Console.ReadLine());
+
+    switch (tipo)
+    {
+        case 1:
+            preco = 5;
+            break;
+        case 2:
+            preco = 3;
+            break;
+        case 3:
+            preco = 7;
+            break;
+    }
+
+    total = horas * preco;
+
+    if (horas > 4)
+    {
+        desconto = total * 0.10;
+        total = total - desconto;
+        Console.WriteLine("Desconto aplicado por ter ficado mais que 4 horas");
+    }
+
+    Console.WriteLine("Total: " + total.ToString("C"));
+}else
+{
+    Console.WriteLine("Digite Horas validas (maior que 0)");
+}
---
+++ menuLanchonete/menuLanchonete/Program.cs
@@ -0,0 +1,51 @@
+﻿int opc, quantidade=0;
+double soma=0;
+
+Console.WriteLine("------ MENU LANCHONETE -----");
+for (int i = 1; i <= 3; i++)
+{
+    Console.WriteLine($"\nPedido {i}º\n1- Hambúrguer ........ R$ 15,00\n2 - Batata Frita ...... R$ 8,00\n3 - Refrigerante ...... R$ 6,00\n4 - Suco Natural ...... R$ 7,00\n5 - Sorvete ........... R$ 10,00\n6 - Sanduíche Natural ........ R$12,00");
+    Console.Write("Escolha uma opção: ");
+    opc = int.Parse(Console.ReadLine());
+    switch (opc)
+    {
+        case 1:
+            Console.Write("Digite a quantidade: ");
+            quantidade = int.Parse(Console.ReadLine());
+            soma = soma + (quantidade * 15);
+            break;
+        case 2:
+            Console.Write("Digite a quantidade: ");
+            quantidade = int.Parse(Console.ReadLine());
+            soma = soma + (quantidade * 8);
+
+            break;
+        case 3:
+            Console.Write("Digite a quantidade: "); 
+            quantidade = int.Parse(Console.ReadLine());
+            soma = soma + (quantidade * 6);
+            break;
+        case 4:
+            Console.Write("Digite a quantidade: ");
+            quantidade = int.Parse(Console.ReadLine());
+            soma = soma + (quantidade * 7);
+
+            break;
+        case 5:
+            Console.Write("Digite a quantidade: ");
+            quantidade = int.Parse(Console.ReadLine());
+            soma = soma + (quantidade * 10);
+            break;
+        case 6:
+            Console.Write("Digite a quantidade: ");
+            quantidade = int.Parse(Console.ReadLine());
+            soma = soma + (quantidade * 12);
+            break;
+        default:
+            Console.WriteLine("\nEscolha uma opção valida!");
+            i = 3;
+            break;
+    }
+}
+
+Console.WriteLine($"\nValor total: {soma.ToString("C")}");
