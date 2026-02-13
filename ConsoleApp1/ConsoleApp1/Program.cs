a13defd78fc15863e4bd0c97ea757b44e02db77b
Mateus Cabral da Silva <131392438+MateusCabralSilva@users.noreply.github.com>
2026-02-13 14:28:59 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿string nome,cidade,uf, tel;
+int anoNasc,idade;
+
+int anoatual=DateTime.Now.Year;
+Console.Write("Digite seu nome: ");
+nome=Console.ReadLine();
+
+Console.Write("Digite sua cidade: ");
+cidade = Console.ReadLine();
+
+Console.Write("Digite sua UF: ");
+uf = Console.ReadLine();
+
+Console.Write("Digite seu ano de nascimento: ");
+anoNasc = int.Parse(Console.ReadLine());
+
+Console.Write("Digite seu telefone: ");
+tel = Console.ReadLine();
+
+idade = anoatual - anoNasc;
+
+Console.WriteLine("Ola! "+nome+" \nReside na cidade: "+cidade+"-UF: "+uf+"\nE tem "+idade+" anos!");
+
+if (idade >= 18)
+{
+    Console.WriteLine("Acesso liberado");
+}else
+{
+    Console.WriteLine("Entrada somente com os pais");
+}
