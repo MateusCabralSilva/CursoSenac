2f09413bc3b6d16b9ab7693a9330cb91c65fd832
Mateus Cabral da Silva <131392438+MateusCabralSilva@users.noreply.github.com>
2026-02-13 14:28:18 -0300

    Add files via upload

---
+++ atividadesWhile/atividadesWhile/Program.cs
@@ -0,0 +1,77 @@
+﻿// 1
+
+int i=1;
+while (i <= 20)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+
+//2
+
+int i;
+Console.Write("Digite um numero: ");
+i = int.Parse(Console.ReadLine());
+
+while (i >= 1)
+{
+    Console.WriteLine(i);
+    i--;
+}
+
+//3
+
+int num, i=1;
+Console.Write("Digite um numero: ");
+num = int.Parse(Console.ReadLine());
+
+while (i <= 10)
+{
+    Console.WriteLine($"{num} x {i} = {num * i}");
+    i++;
+}
+
+//4
+
+int num,i = 1;
+
+Console.Write("Digite um numero: ");
+num = int.Parse(Console.ReadLine());
+
+while (i <= 8)
+{
+    Console.WriteLine(i + num);
+    i++;
+}
+
+//5
+
+string nome, email;
+int i=1,cpf, idade, telefone;
+
+while (i <= 5)
+{
+    Console.WriteLine($"\n-------- PESSOA {i}º --------");
+    Console.Write("Digite seu Nome: ");
+    nome = Console.ReadLine();
+
+    Console.Write("Digite sua idade: ");
+    idade = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite seu E-mail: ");
+    email = Console.ReadLine();
+
+    Console.Write("Digite seu cpf: ");
+    cpf = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite seu telefone: ");
+    telefone = int.Parse(Console.ReadLine());
+
+    if (idade >= 60)
+    {
+        Console.WriteLine("\n-> Dar desconto");
+    }
+
+    i++;
+}
